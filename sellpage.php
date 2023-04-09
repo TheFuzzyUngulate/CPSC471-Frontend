d484698e2c6428a9df63a57f03a4410f97f6ced3
LC <luke.chen@ucalgary.ca>
2023-04-08 18:00:02 -0600

    modified main.css to support sellpage.php and modified sellpage.php

--- sellpage.php
+++ sellpage.php
@@ -40,30 +40,32 @@
 	<div class="sellpageforms">
 	    "Please enter details of your listing:"
             <form id="sellpage-sellhome-form" class="sellform" action="" method ="POST" accept-charset="utf-8">
-                <input type="text" class="main-searchbar-input" placeholder="Address" name="q">
-                <button class="main-searchbar-button" type="submit"></button>
+                <input type="text" class="main-searchbar-input" placeholder="Address" name="address">
+                <button class="submit-sellform-button" type="submit" value="submit">Submit</button>
             </form>
        	    <form id="sellpage-sellhome-form2" class="sellform" action="" method ="POST" accept-charset="utf-8">
-                <input type="text" class="main-searchbar-input" placeholder="Price" name="q">
-                <button class="main-searchbar-button" type="submit"></button>
+                <input type="text" class="main-searchbar-input" placeholder="Price" name="price">
+                <button class="submit-sellform-button" type="submit" value="submit">Submit</button>
             </form>
  	    <form id="sellpage-sellhome-form3" class="sellform" action="" method ="POST" accept-charset="utf-8">
-                <input type="text" class="main-searchbar-input" placeholder="No. of full bathrooms" name="q">
-                <button class="main-searchbar-button" type="submit"></button>
+                <input type="text" class="main-searchbar-input" placeholder="No. of full bathrooms" name="baths">
+                <button class="submit-sellform-button" type="submit" value="submit">Submit</button>
             </form>
 	    <form id="sellpage-sellhome-form3" class="sellform" action="" method ="POST" accept-charset="utf-8">
-                <input type="text" class="main-searchbar-input" placeholder="No. of half baths" name="q">
-                <button class="main-searchbar-button" type="submit"></button>
+                <input type="text" class="main-searchbar-input" placeholder="No. of half baths" name="halfbaths">
+                <button class="submit-sellform-button" type="submit" value="submit">Submit</button>
             </form>
 	    <form id="sellpage-sellhome-form4" class="sellform" action="" method ="POST" accept-charset="utf-8">
-                <input type="text" class="main-searchbar-input" placeholder="No. of bedrooms" name="q">
-                <button class="main-searchbar-button" type="submit"></button>
+                <input type="text" class="main-searchbar-input" placeholder="No. of bedrooms" name="bedrooms">
+                <button class="submit-sellform-button" type="submit" value="submit">Submit</button>
             </form>
 	    <form id="sellpage-sellhome-form5" class="sellform" action="" method ="POST" accept-charset="utf-8">
-                <input type="text" class="main-searchbar-input" placeholder="Description" name="q">
-                <button class="main-searchbar-button" type="submit"></button>
+                <input type="text" class="main-searchbar-input" placeholder="Description" name="description">
+                <button class="submit-sellform-button" type="submit" value="submit">Submit</button>
             </form>
         </div>
     </div>
+	// Access values of the form inputs using $_POST['{name}'] e.g., <?php echo $_POST['description'] ?>
+	// User must press submit before value is submitted, should check if value is empty/null before proceeding
 </body>
 </html>
